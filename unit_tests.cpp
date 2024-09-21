7bb605c834fa8b6d568edafea14fa9bc6e9de7d0
KC Lopez <khalelclarklopez@gmail.com>
2024-09-20 21:38:24 -0700

    added more test cases

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -9,3 +9,74 @@ TEST_CASE("A new stack is empty", "testTag1")
     REQUIRE(tester.empty() == true);
     REQUIRE(tester.full() == false);
 }
+
+TEST_CASE("A full stack won't overflow")
+{
+    MyStack tester;
+
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(0) == false);
+}
+
+TEST_CASE("A full stack is detected")
+{
+    MyStack tester;
+
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.full() == true);
+}
+
+TEST_CASE("A full stack is printed without overflowing values")
+{
+    MyStack tester;
+
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(2) == true);
+    REQUIRE(tester.push(3) == true);
+    REQUIRE(tester.push(4) == true);
+    REQUIRE(tester.push(5) == true);
+    REQUIRE(tester.push(6) == true);
+    REQUIRE(tester.push(7) == true);
+    REQUIRE(tester.push(8) == true);
+    REQUIRE(tester.push(9) == true);
+    REQUIRE(tester.push(0) == true);
+    tester.push(0);
+    REQUIRE_THAT(tester.print(), Catch::Equals("1 2 3 4 5 6 7 8 9 0 "));
+}
+
+TEST_CASE("A popped value will cause the one below to return for 'top()'")
+{
+    MyStack tester;
+
+    REQUIRE(tester.push(1) == true);
+    REQUIRE(tester.push(2) == true);
+    tester.pop();
+    REQUIRE(tester.top() == 1);
+}
+
+TEST_CASE("An empty pop won't affect the index of new values")
+{
+    MyStack tester;
+
+    tester.pop();
+    REQUIRE(tester.push(1));
+    REQUIRE(tester.top() == 1);
+}
